29a131b15935256bc6d6047df004438ae6d86d66
Aman Sood <38162730+AmanSoodprog@users.noreply.github.com>
2023-05-15 00:35:47 +0530

    Add files via upload

---
+++ rateHappiness/DB_connect.py
@@ -0,0 +1,8 @@
+from pymongo import MongoClient
+myclient = MongoClient("mongodb+srv://Pixel:Pixel7788@cluster0.3dpfxx3.mongodb.net/mydb?retryWrites=true&w=majority")
+db_name = myclient["DB069"]
+
+
+
+
+
